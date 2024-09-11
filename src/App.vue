6088a3e822394cb6490adb87dcb733508ef86848
JBdevCristian <jbcristian78@gmail.com>
2024-09-11 12:18:46 -0300

    Consumo de API VUE

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <div class="column is-half is-offset-one-quarter">
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <h4 class="is-size-2">Pokedex</h4>
+      <input type="text" class="input is-rounded" placeholder="Buscar Pokemon pelo nome" v-model="busca"/>
+      <button class="button is-fullwidth is-success" id="botForm">Buscar</button>
+      <div v-for="(poke, index) in resultadoBusca" :key="index">
      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
     </div>
     </div>
@@ -16,7 +19,8 @@ export default {
   name: 'App',
   data() {
     return{
-      pokemons: []
+      pokemons: [],
+      busca: ''
     }
   },
     created: function() {
@@ -27,6 +31,17 @@ export default {
     },
     components: {
       Pokemon
+    },
+    computed: {
+      resultadoBusca: function() {
+    if (this.busca === '') {
+      return this.pokemons;
+    } else {
+      return this.pokemons.filter(pokemon => 
+        pokemon.name.toLowerCase().includes(this.busca.toLowerCase())
+      );
+    }
+  }
     }
   
 }
@@ -40,5 +55,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#botForm {
+  margin-top: 2%;
 }
 </style>
